0d46475ddd49f224ac63da3a0828f7ff1c3fb73a
ashish-ad <ashish.pradhan@vit.edu.in>
2021-08-14 20:35:41 +0530

    This the point I have been stuck from last three days from 11th of august 2021

--- Compititave_programing/sereja_and_dima.cpp
+++ Compititave_programing/sereja_and_dima.cpp
@@ -10,10 +10,24 @@ int main(){
     cin>>n;
     vector <int> cards(n);
 
-    for (auto & i:cards){
+    for (int & i:cards){
         cin>>i;
     }
+    for (int i: cards){
+        cout<<i<<"\t";
+    }cout<<endl;
+
     
+    vector <int> ::iterator p1;
+    vector <int> ::iterator p2;
+    vector <int> sereja;
+    vector <int> dima;
+    for (int i:sereja){
+        cout<<i<<endl;
+    }
+    for (int i:dima){
+        cout<<i<<endl;
+    }
     return 0;
 }
     
